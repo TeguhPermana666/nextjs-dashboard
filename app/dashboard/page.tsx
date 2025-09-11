defffa0177bfd39c551793f43bc77dbd82de56dc
TeguhPermana666 <teguhpermana096@gmail.com>
2025-09-11 17:10:23 +0800

    bab 4 lanjutan

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,11 @@
 export default function Page(){
     return (
+<<<<<<< HEAD
         <div>
             <h1>Dashboard Page</h1>
         </div>
+=======
+        <p>Dashboard Page</p>
+>>>>>>> 5b5cbc4 (bab 4 lanjutan)
     )
 }

03b4c4ead4fbfba9091afe41c193af4d977022d1
TeguhPermana666 <teguhpermana096@gmail.com>
2025-10-03 11:50:37 +0800

    Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,25 @@
-export default function Page(){
-    return (
-        <div>
-            <h1>Customers Page</h1>
-        </div>
-    )
-} 
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+    title: 'Customers',
+}
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  
+  // 1. Memanggil fungsi data dari page.tsx untuk mengambil SEMUA customer yang cocok
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      {/* 2. Mengirim data 'customers' sebagai prop ke komponen CustomersTable */}
+      <CustomersTable customers={customers} />
+    </main>
+  );
+}
